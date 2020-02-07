0617952d88327ad69b5396e6d54b0da0ebdcdcac
MasterOfMenace <nero_wolf@mail.ru>
2020-02-07 18:08:08 +0300

    Добавил в компоненте WelcomeScreen обработчик нажатия кнопки, добавил e2e тест для WelcomeScreen

--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -3,7 +3,11 @@ import renderer from 'react-test-renderer';
 import WelcomeScreen from "./welcome-screen.jsx";
 
 it(`Should WelcomeScreen render correctly`, () => {
-  const tree = renderer.create(<WelcomeScreen errorsCount={3}/>).toJSON();
+  const tree = renderer
+    .create(<WelcomeScreen
+      errorsCount={3}
+      welcomeButtonClickHandler={()=>{}}/>)
+    .toJSON();
 
   expect(tree).toMatchSnapshot();
 });
