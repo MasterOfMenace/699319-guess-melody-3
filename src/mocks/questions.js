771e33975c955b24a28ddf46bbd6f445f721aa1e
MasterOfMenace <nero_wolf@mail.ru>
2020-02-11 22:39:08 +0300

    Создал моки с вопросами и ответами

---
+++ src/mocks/questions.js
@@ -0,0 +1,48 @@
+const ARTIST_PICTURE_URL = `https://api.adorable.io/avatars/128`;
+
+export default [
+  {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    },
+    answers: [
+      {
+        artist: `John Snow`,
+        picture: `${ARTIST_PICTURE_URL}/${Math.random()}`,
+      },
+      {
+        artist: `Ozzy Osbourne`,
+        picture: `${ARTIST_PICTURE_URL}/${Math.random()}`,
+      },
+      {
+        artist: `Jim Beam`,
+        picture: `${ARTIST_PICTURE_URL}/${Math.random()}`,
+      }
+    ],
+  },
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        genre: `rock`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      },
+      {
+        genre: `blues`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      },
+      {
+        genre: `jazz`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      },
+      {
+        genre: `folk-metal`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      }
+    ],
+  },
+];
+
