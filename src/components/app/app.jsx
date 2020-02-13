3160215a6a446ce50bfb7b8be1e9525ef04f5f14
MasterOfMenace <nero_wolf@mail.ru>
2020-02-13 19:35:14 +0300

    Переписал компонент App на классы, добавил состояние, описал логику переключения экранов, обновил тесты для App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,28 +1,91 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {GameType} from '../../const.js';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 import QuestionArtistScreen from '../question-artist-screen/question-artist-screen.jsx';
 import QuestionGenreScreen from '../question-genre-screen/question-genre-screen.jsx';
-const welcomeButtonHandler = () => {};
 
-const App = ({errorsCount, questions}) => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <WelcomeScreen errorsCount={errorsCount} welcomeButtonClickHandler={welcomeButtonHandler} />
-        </Route>
-        <Route exact path="/dev-artist">
-          <QuestionArtistScreen question={questions[0]}/>
-        </Route>
-        <Route exact path="/dev-genre">
-          <QuestionGenreScreen question={questions[1]} />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: -1
+    };
+  }
+
+  _renderGameScreen() {
+    const {errorsCount, questions} = this.props;
+    const step = this.state;
+    const question = questions[step];
+
+    if (step === -1 || step >= questions.length) {
+      return (
+        <WelcomeScreen
+          errorsCount={errorsCount}
+          onWelcomeButtonClick={()=>{
+            this.setState({
+              step: 0
+            });
+          }}/>
+      );
+    }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <QuestionArtistScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1
+                }));
+              }}
+            />
+          );
+        case GameType.GENRE:
+          return (
+            <QuestionGenreScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1
+                }));
+              }}
+            />
+          );
+      }
+    }
+
+    return null;
+  }
+
+  render() {
+    const {questions} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path="/dev-artist">
+            <QuestionArtistScreen
+              question={questions[0]}
+              onAnswer={()=>{}}/>
+          </Route>
+          <Route exact path="/dev-genre">
+            <QuestionGenreScreen
+              question={questions[1]}
+              onAnswer={()=>{}}/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,8 +2,58 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './app.jsx';
 
+const questions = [
+  {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    },
+    answers: [
+      {
+        artist: `John Snow`,
+        picture: `https://api.adorable.io/avatars/128/1`,
+      },
+      {
+        artist: `Ozzy Osbourne`,
+        picture: `https://api.adorable.io/avatars/128/2`,
+      },
+      {
+        artist: `Jim Beam`,
+        picture: `https://api.adorable.io/avatars/128/3`,
+      }
+    ],
+  },
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        genre: `rock`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      },
+      {
+        genre: `blues`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      },
+      {
+        genre: `jazz`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      },
+      {
+        genre: `rock`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      }
+    ],
+  },
+];
+
 it(`Render App`, () => {
-  const tree = renderer.create(<App errorsCount={3}/>).toJSON();
+  const tree = renderer
+    .create(<App
+      errorsCount={3}
+      questions={questions}/>)
+    .toJSON();
 
   expect(tree).toMatchSnapshot();
 });
