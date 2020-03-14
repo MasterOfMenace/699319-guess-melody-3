710f1ae5f3dcd24150d8e354aa9e1f9edd3cdf9f
MasterOfMenace <nero_wolf@mail.ru>
2020-03-14 13:01:38 +0300

    НАписал е2е тест для хока withAudio, удалил лишний закомментированный код в компоненте AudioPlayer

--- src/components/audio-player/audio-player.jsx
+++ src/components/audio-player/audio-player.jsx
@@ -2,58 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AudioPlayer extends React.PureComponent {
-  // constructor(props) {
-  //   super(props);
-
-  //   this._audioRef = createRef();
-
-  //   this.state = {
-  //     progress: 0,
-  //     isLoading: true,
-  //     isPlaying: props.isPlaying,
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   if (!this._audioRef.current) {
-  //     return;
-  //   }
-
-  //   const {src} = this.props;
-  //   const audio = this._audioRef.current;
-
-  //   audio.src = src;
-
-  //   audio.oncanplaythrough = () => this.setState({
-  //     isLoading: false
-  //   });
-
-  //   audio.onplay = () => this.setState({
-  //     isPlaying: true
-  //   });
-
-  //   audio.onpause = () => this.setState({
-  //     isPlaying: false
-  //   });
-
-  //   audio.ontimeupdate = () => this.setState({
-  //     progress: audio.currentTime
-  //   });
-  // }
-
-  // componentWillUnmount() {
-  //   if (!this._audioRef.current) {
-  //     return;
-  //   }
-  //   const audio = this._audioRef.current;
-
-  //   audio.oncanplaythrough = null;
-  //   audio.onplay = null;
-  //   audio.onpause = null;
-  //   audio.ontimeupdate = null;
-  //   audio.src = ``;
-  // }
-
   render() {
     const {isLoading, isPlaying, onPlayButtonClick, children} = this.props;
 
