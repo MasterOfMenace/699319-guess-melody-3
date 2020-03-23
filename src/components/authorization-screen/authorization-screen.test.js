87fb898a868d5cc5833a9a4b0ea056afecde2ea6
MasterOfMenace <nero_wolf@mail.ru>
2020-03-23 21:48:09 +0300

    Создал компонент AuthorizationScreen и снепшот тест для него

---
+++ src/components/authorization-screen/authorization-screen.test.js
@@ -0,0 +1,14 @@
+import React from 'React';
+import renderer from 'react-test-renderer';
+import AuthorizationScreen from './authorization-screen.jsx';
+
+it(`Правильное отображение компонента AuthorizationScreen`, () => {
+  const tree = renderer
+    .create(
+        <AuthorizationScreen
+          onReplayButtonClick={()=>{}}
+        />
+    ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
